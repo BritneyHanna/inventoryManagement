c18294eda859346633c1bfdb9aba2289ba378c7d
BritneyHanna <b.ahanna@yahoo.com>
2020-11-22 21:11:36 -0500

    added controller and App class

--- src/main/java/com/promineotech/inventoryManagement/App.java
+++ src/main/java/com/promineotech/inventoryManagement/App.java
@@ -1,13 +1,13 @@
 package com.promineotech.inventoryManagement;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.promineotech.inventoryManagement")
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
